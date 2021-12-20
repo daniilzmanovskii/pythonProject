042eabb2790326f43f589d20c8731e0253e1c807
Даниил <danyatop@bk.ru>
2021-12-21 02:31:48 +0800

    edit

--- main.py
+++ main.py
@@ -1,128 +1,66 @@
+import random
+
 import pygame
 
 pygame.init()
-win = pygame.display.set_mode((800, 600))
 
+display_width = 800
+display_height = 600
+
+display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Star Dodge')
+fon1 = pygame.image.load('darkPurple.png')
+player1 = pygame.image.load('playerShip1_red.png')
 
-bg = pygame.image.load('darkPurple.png')
-playerStand = pygame.image.load('playerShip1_blue.png')
-meteorMB = pygame.image.load('MBrown.png')
-meteorSB = pygame.image.load('SBrown.png')
-meteorBB = pygame.image.load('BBrown.png')
-meteorBG = pygame.image.load('BGrey.png')
-meteorSG = pygame.image.load('SGrey.png')
-meteorMG = pygame.image.load('MGrey.png')
+width = 60
+height = 100
+x = display_width // 3
+y = display_height - height - 100
 
-x = 350
-y = 480
-width = 99
-height = 75
-speed = 10
+c_x = display_width - 50
+c_y = display_height - 20 - 100
+c_w = 20
+c_h = 70
 
-MeteorBBMoveRight_size_x = 20
-MeteorBBMoveRight_size_y = 0
-MeteorBBMoveRight_position_x = 800 - 50
-MeteorBBMoveRight_position_y = 600 - 550
-MeteorBBMoveRight_speed_x = 8
-MeteorBBMoveRight_speed_y = 10
 
-MeteorSBMoveRight_size_x = 20
-MeteorSBMoveRight_size_y = 0
-MeteorSBMoveRight_size_x = 450 - 50
-MeteorSBMoveRight_size_y = 600 - 480
-MeteorSBMoveRight_speed_x = 12
-MeteorSBMoveRight_speed_y = 10
+class Cactus():
+    def __init__(self, x1, y1, width1, height1, speed_x, speed_y, image):
+        self.x = x1
+        self.y = y1
+        self.width = width1
+        self.height = height1
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        self.image = image
 
-MeteorMGMoveRight_size_x = 20
-MeteorMGMoveRight_size_y = 0
-MeteorMGMoveRight_position_x = 150 - 50
-MeteorMGMoveRight_position_y = 400 - 40
-MeteorMGMoveRight_speed_x = 9
-MeteorMGMoveRight_speed_y = 7
+    def move_left(self):
+        display.blit(self.image, (self.x, self.y))
+        if self.x >= -self.width:
+            self.x -= self.speed_x
+        else:
+            self.x = display_width - 50
 
-MeteorBGMoveRight_size_x = 20
-MeteorBGMoveRight_size_y = 0
-MeteorBGMoveRight_position_x = 80
-MeteorBGMoveRight_position_y = 50
-MeteorBGMoveRight_speed_x = 11
-MeteorBGMoveRight_speed_y = 9
+        if self.y >= self.height - 150 and (self.y < 650):
+            self.y += self.speed_y
+        else:
+            self.y = 0
 
-MeteorMBMoveRight_size_x = 20
-MeteorMBMoveRight_size_y = 0
-MeteorMBMoveRight_position_x = 400
-MeteorMBMoveRight_position_y = 200
-MeteorMBMoveRight_speed_x = 8
-MeteorMBMoveRight_speed_y = 15
+    def move_right(self):
+        display.blit(self.image, (self.x, self.y))
+        if self.x <= -self.width + 800:
+            self.x += self.speed_x
+        else:
+            self.x = 50
 
-def moveCactusRight1():
-    global MeteorMBMoveRight_size_y, MeteorMBMoveRight_position_y, MeteorMBMoveRight_position_x, MeteorMBMoveRight_size_x
-
-    if MeteorMBMoveRight_position_x <= MeteorBGMoveRight_size_x + 800:
-        MeteorMBMoveRight_position_x += 8
-    else:
-        MeteorMBMoveRight_position_x = 50
-
-    if MeteorMBMoveRight_position_y >= MeteorBGMoveRight_size_y - 50 and (MeteorMBMoveRight_position_y < 650):
-        MeteorMBMoveRight_position_y += 15
-    else:
-        MeteorMBMoveRight_position_y = 0
-
-def moveCactusRight():
-    global MeteorBGMoveRight_size_y, MeteorBGMoveRight_position_y, MeteorBGMoveRight_position_x, MeteorBGMoveRight_size_x
-
-    if MeteorBGMoveRight_position_x <= MeteorBGMoveRight_size_x + 800:
-        MeteorBGMoveRight_position_x += 11
-    else:
-        MeteorBGMoveRight_position_x = 50
-
-    if MeteorBGMoveRight_position_y >= MeteorBGMoveRight_size_y - 50 and (MeteorBGMoveRight_position_y < 650):
-        MeteorBGMoveRight_position_y += 9
-    else:
-        MeteorBGMoveRight_position_y = 0
-
-def moveCactusLeft():
-    global MeteorBBMoveRight_size_y, MeteorBBMoveRight_position_y, MeteorBBMoveRight_position_x, MeteorBBMoveRight_size_x
-
-    if MeteorBBMoveRight_position_x >= MeteorBBMoveRight_size_x:
-        MeteorBBMoveRight_position_x -= MeteorBBMoveRight_speed_x
-    else:
-        MeteorBBMoveRight_position_x = 800 - 50
-
-    if MeteorBBMoveRight_position_y >= MeteorBBMoveRight_size_y - 50 and (MeteorBBMoveRight_position_y < 650):
-        MeteorBBMoveRight_position_y += MeteorBBMoveRight_speed_y
-    else:
-        MeteorBBMoveRight_position_y = 0
-
-def moveCactusLeft1():
-    global MeteorMGMoveRight_size_y, MeteorSBMoveRight_size_y, MeteorSBMoveRight_size_x, MeteorMGMoveRight_size_x
-
-    if MeteorSBMoveRight_size_x >= MeteorMGMoveRight_size_x:
-        MeteorSBMoveRight_size_x -= 12
-    else:
-        MeteorSBMoveRight_size_x = 800 - 50
-
-    if MeteorSBMoveRight_size_y >= MeteorMGMoveRight_size_y - 50 and (MeteorSBMoveRight_size_y < 650):
-        MeteorSBMoveRight_size_y += 10
-    else:
-        MeteorSBMoveRight_size_y = 0
-
-def moveCactusLeft2():
-    global MeteorMGMoveRight_size_y, MeteorMGMoveRight_position_y, MeteorMGMoveRight_position_x, MeteorMGMoveRight_size_x
-
-    if MeteorMGMoveRight_position_x >= MeteorMGMoveRight_size_x:
-        MeteorMGMoveRight_position_x -= 9
-    else:
-        MeteorMGMoveRight_position_x = 800 - 50
-
-    if MeteorMGMoveRight_position_y >= MeteorMGMoveRight_size_y - 50 and (MeteorMGMoveRight_position_y < 650):
-        MeteorMGMoveRight_position_y += 7
-    else:
-        MeteorMGMoveRight_position_y = 0
+        if self.y >= self.height - 120 and (self.y < 650):
+            self.y += self.speed_y
+        else:
+            self.y = 0
 
 
 font = pygame.font.SysFont('microsofttaile', 32)
 follow = font.render('Pause! Press ENTER to continue', 1, (173, 255, 47))
+
 
 def pause():
     paused = True
@@ -132,7 +70,7 @@ def pause():
                 pygame.quit()
                 quit()
 
-        win.blit(follow, (180, 270))
+        display.blit(follow, (180, 270))
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RETURN]:
@@ -141,17 +79,46 @@ def pause():
         pygame.display.update()
 
 
-def drawWindow():
-    win.blit(bg, (0, 0))
-    win.blit(playerStand, (x, y))
-    win.blit(meteorBB, (MeteorBBMoveRight_position_x, MeteorBBMoveRight_position_y))
-    win.blit(meteorSB, (MeteorSBMoveRight_size_x, MeteorSBMoveRight_size_y))
-    win.blit(meteorMG, (MeteorMGMoveRight_position_x, MeteorMGMoveRight_position_y))
-    win.blit(meteorBG, (MeteorBGMoveRight_position_x, MeteorBGMoveRight_position_y))
-    win.blit(meteorMB, (MeteorMBMoveRight_position_x, MeteorMBMoveRight_position_y))
-    pygame.display.update()
+width = 60
+height = 100
+x = display_width // 3
+y = display_height - height - 30
+
+c_x = display_width - 50
+c_y = display_height - 170
+c_w = 20
+c_h = 70
+
+cactus_arr_left = []
+cactus_arr_right = []
+
+
+def create_cactus_arr(array):
+    array.append(Cactus(display_width - 50, display_height, 101, 84, 4, 5, pygame.image.load('BBrown.png')))
+    array.append(Cactus(display_width - 500, display_height + 43, 43, 50, 3, 4, pygame.image.load('MBrown.png')))
+    array.append(Cactus(display_width - 300, display_height + 100, 28, 28, 6, 4, pygame.image.load('SGrey.png')))
+
+
+def draw_array(array):
+    for cactus in array:
+        cactus.move_left()
+
+
+def create_cactus_arrR(array):
+    array.append(Cactus(display_width, display_height, 120, 98, 6, 6, pygame.image.load('BGrey.png')))
+    array.append(Cactus(display_width - 500, display_height + 50, 28, 28, 4, 7, pygame.image.load('SBrown.png')))
+    array.append(Cactus(display_width - 300, display_height + 100, 45, 40, 5, 2, pygame.image.load('MGrey.png')))
+
+
+def draw_arrayR(array):
+    for cactus in array:
+        cactus.move_right()
+
 
 run = True
+create_cactus_arr(cactus_arr_left)
+create_cactus_arrR(cactus_arr_right)
+
 while run:
     pygame.time.delay(35)
 
@@ -161,20 +128,15 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x > 15:
-        x -= speed
+        x -= 15
     if keys[pygame.K_RIGHT] and x < 785 - width:
-        x += speed
+        x += 15
     if keys[pygame.K_ESCAPE]:
         pause()
 
-    moveCactusRight1()
-    moveCactusRight()
-    moveCactusLeft2()
-    moveCactusLeft1()
-    moveCactusLeft()
-    drawWindow()
+    display.blit(fon1, (0, 0))
+    draw_array(cactus_arr_left)
+    draw_arrayR(cactus_arr_right)
 
-
-
-pygame.quit()
-
+    display.blit(player1, (x, y))
+    pygame.display.update()
