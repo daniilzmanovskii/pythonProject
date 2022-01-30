62f0fbcc3d0147f71188f4ec2fec7dd2b829ec80
Даниил <danyatop@bk.ru>
2022-01-30 21:14:06 +0800

    edit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from Meteor import Meteor
 import pygame_menu
 
 pygame.init()
@@ -16,13 +17,20 @@ player1 = pygame.image.load('playerShip1_red.png')
 
 width = 60
 height = 100
-x = display_width // 3
+x = display_width // 2 - 50
 y = display_height - height - 50
 
 c_x = display_width - 50
 c_y = display_height - 20 - 100
 c_w = 20
 c_h = 70
+
+meteors = pygame.sprite.Group
+
+meteors.add(Meteor(display_width // 2, 10, 'BBrown.png'))
+#meteors.add(Meteor(display_width - 50, 5, 'MGrey.png'))
+#meteors.add(Meteor(display_width - 400, 7, 'MBrown.png'))
+#meteors.add(Meteor(display_width, 9, 'BGrey.png'))
 
 
 def terminate():
@@ -35,41 +43,6 @@ def start_menu(theme=pygame_menu.themes.THEME_BLUE):
     menu.add.button('PLAY', start_game)
     menu.add.button('EXIT', pygame_menu.events.EXIT)
     menu.mainloop(display)
-
-
-class Cactus(pygame.sprite.Sprite):
-    def __init__(self, x1, y1, width1, height1, speed_x, speed_y, image):
-        self.x = x1
-        self.y = y1
-        self.width = width1
-        self.height = height1
-        self.speed_x = speed_x
-        self.speed_y = speed_y
-        self.image = image
-
-    def move_left(self):
-        display.blit(self.image, (self.x, self.y))
-        if self.x >= -self.width:
-            self.x -= self.speed_x
-        else:
-            self.x = display_width - 50
-
-        if self.y >= self.height - 150 and (self.y < 650):
-            self.y += self.speed_y
-        else:
-            self.y = 0
-
-    def move_right(self):
-        display.blit(self.image, (self.x, self.y))
-        if self.x <= -self.width + 800:
-            self.x += self.speed_x
-        else:
-            self.x = 50
-
-        if self.y >= self.height - 120 and (self.y < 650):
-            self.y += self.speed_y
-        else:
-            self.y = 0
 
 
 font = pygame.font.SysFont('microsofttaile', 32)
@@ -97,31 +70,7 @@ cactus_arr_left = []
 cactus_arr_right = []
 
 
-def create_cactus_arr(array):
-    array.append(Cactus(display_width - 50, display_height, 101, 84, 4, 5, pygame.image.load('BBrown.png')))
-    array.append(Cactus(display_width - 500, display_height + 43, 43, 50, 3, 4, pygame.image.load('MBrown.png')))
-    array.append(Cactus(display_width - 300, display_height + 100, 28, 28, 6, 4, pygame.image.load('SGrey.png')))
-
-
-def draw_array(array):
-    for cactus in array:
-        cactus.move_left()
-
-
-def create_cactus_arrR(array):
-    array.append(Cactus(display_width, display_height, 120, 98, 6, 6, pygame.image.load('BGrey.png')))
-    array.append(Cactus(display_width - 500, display_height + 50, 28, 28, 4, 7, pygame.image.load('SBrown.png')))
-    array.append(Cactus(display_width - 300, display_height + 100, 45, 40, 5, 2, pygame.image.load('MGrey.png')))
-
-
-def draw_arrayR(array):
-    for cactus in array:
-        cactus.move_right()
-
-
 run = True
-create_cactus_arr(cactus_arr_left)
-create_cactus_arrR(cactus_arr_right)
 
 
 font1 = pygame.font.SysFont('microsofttaile', 32)
@@ -173,11 +122,11 @@ def start_game():
             pause()
 
         display.blit(fon1, (0, 0))
-        draw_array(cactus_arr_left)
-        draw_arrayR(cactus_arr_right)
-
         display.blit(player1, (x, y))
+        meteors.draw(display)
         pygame.display.update()
+
+        meteors.update(display_height)
 
 
 start_menu()
